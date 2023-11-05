8223863ad3ee81fe03884e530697c40525ec8905
Thomas Wallsmith <90111415+twallie@users.noreply.github.com>
2023-11-05 22:06:47 +0000

    refactor: added sidenav component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,37 +1,13 @@
 <script lang="ts">
 	import '../app.css';
 	import Header from '$lib/components/Header.svelte';
-	import ClickableInfoBox from '$lib/components/ClickableInfoBox.svelte';
+	import SidenavMenu from '$lib/components/SidenavMenu.svelte';
 
-	import XSymbol from '$lib/images/x.svg';
-
-	let sidenavClicked: boolean;
-
-	const scrollToHokkaiDough = () => {
-		const element = document.getElementById('hokkaidough');
-		if (element == null) {
-			return;
-		}
-		element.scrollIntoView();
-		sidenavClicked = false;
-	};
-
-	const closeSidenav = () => {
-		sidenavClicked = false;
-	};
+	let sidenavClicked: boolean = false;
 </script>
 
 {#if sidenavClicked}
-	<div class="flex flex-col fixed w-60 top-0 right-0 bg-white/75 h-full">
-		<button on:click={closeSidenav} class="w-[50px] ml-auto mt-10 mr-5 mb-5">
-			<img src={XSymbol} alt="Close" />
-		</button>
-		<ClickableInfoBox textColor="black" title="Order Now" />
-		<ClickableInfoBox onClick={scrollToHokkaiDough} textColor="black" title="HokkaiDough" />
-		<ClickableInfoBox textColor="black" title="Find Us" />
-		<ClickableInfoBox textColor="black" title="Full Menu" />
-		<ClickableInfoBox textColor="black" title="FAQ" />
-	</div>
+	<SidenavMenu bind:sidenavClicked />
 {/if}
 <Header bind:sidenavClicked />
 
