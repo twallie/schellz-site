5eaa4a24b7a16123fcde10aea0c7c8ade443aca6
Thomas Wallsmith <90111415+twallie@users.noreply.github.com>
2023-11-14 05:41:38 +0000

    feat: added Contact Us to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,11 @@
 	import HomepageOrderOptions from '$lib/components/HomepageOrderOptions.svelte';
 	import HomepageHero from '$lib/components/HomepageHero.svelte';
 	import HomepageFaq from '$lib/components/HomepageFaq.svelte';
+	import HomepageContact from '$lib/components/HomepageContact.svelte';
 </script>
 
 <HomepageHero />
 <HomepageOrderOptions />
 <HomepageHokkaiDough />
 <HomepageFaq />
+<HomepageContact />
