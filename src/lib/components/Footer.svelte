414b7cf4d2ed74ff1032b794a37446505eb24817
Thomas Wallsmith <90111415+twallie@users.noreply.github.com>
2023-11-14 06:17:20 +0000

    refactor: footer component

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import Logo from '$lib/images/logo.png';
+</script>
+
+<footer class="mt-10 bg-black flex flex-col text-center py-5">
+	<img src={Logo} class="w-1/2 mx-auto" alt="Schells Logo" />
+	<p class="text-white text-sm pt-5">© 2023 ALL RIGHT RESERVED.</p>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,7 @@
 	import '../app.css';
 	import Header from '$lib/components/Header.svelte';
 	import StaticHeader from '$lib/components/StaticHeader.svelte';
-
-	//
-	import Logo from '$lib/images/logo.png';
-	//
+	import Footer from '$lib/components/Footer.svelte';
 
 	let sidenavClicked: boolean = false;
 	let scrollY = 0;
@@ -16,12 +13,7 @@
 
 <Header bind:sidenavClicked bind:visible={topOfPage} />
 <StaticHeader bind:sidenavClicked bind:topOfPage />
-
 <slot />
-
-<footer class="mt-10 bg-black flex flex-col text-center py-5">
-	<img src={Logo} class="w-1/2 mx-auto" alt="Schells Logo" />
-	<p class="text-white text-sm pt-5">© 2023 ALL RIGHT RESERVED.</p>
-</footer>
+<Footer />
 
 <svelte:window bind:scrollY />
