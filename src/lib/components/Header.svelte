55df4114337e60da74e94e1af3acb8faff5d272f
Thomas Wallsmith <90111415+twallie@users.noreply.github.com>
2023-11-14 02:42:48 +0000

    fix: bug where close/open navbar would flicker

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -6,27 +6,24 @@
 
 	export let sidenavClicked = false;
 	export let visible = true;
-
+s
 	const flipSidenavValue = () => {
 		sidenavClicked = !sidenavClicked;
 	};
 </script>
 
 <div
-	class="flex flex-row justify-between align-middle {visible
-		? 'visible'
-		: 'invisible'} bg-slate-200 pb-1 bg-opacity-[95%] shadow-2xl"
+	class="flex flex-row justify-between align-middle
+	{visible ? 'visible' : 'invisible'}
+	bg-slate-200 pb-1 bg-opacity-[95%] shadow-2xl"
 >
 	<div class="w-1/2">
 		<img src={Logo} alt="Schellz's Logo" />
 	</div>
 	<div class="h-[50px] w-[50px] my-auto ml-auto mr-5">
 		<button on:click={flipSidenavValue}>
-			{#if !sidenavClicked}
-				<img src={Sidenav} alt="sidenav button" />
-			{:else}
-				<img src={XSymbol} alt="sidenav button" />
-			{/if}
+			<img class={sidenavClicked ? 'hidden' : ''} src={Sidenav} alt="Open sidenav button" />
+			<img class={!sidenavClicked ? 'hidden' : ''} src={XSymbol} alt="Close sidenav button" />
 		</button>
 	</div>
 </div>
