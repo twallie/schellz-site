4deabcf2f4120d75082fe30e17fc34ab5eb5c15f
Thomas Wallsmith <90111415+twallie@users.noreply.github.com>
2023-11-14 02:35:40 +0000

    refactor: homepage faq component

---
+++ src/lib/components/HomepageFaq.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import HomepageInfoBox from './HomepageInfoBox.svelte';
+	import type { FAQItem } from '$lib/types';
+	const faqItems: FAQItem[] = [
+		{
+			question: 'How is the parking?',
+			answer:
+				'Please park in the front parking lot or on the street. Head to the front entrance and select "Schellz Pizza Co" on the call box. Our kitchen is down the hall #7.'
+		},
+		{
+			question: 'How do I reheat Schellz pizzas?',
+			answer:
+				'Due to our long fermentation and our style of dough, you will be surprised to know that Schellz Pizza is quite delicious 2-3 days after purchase.\n\nJust reheat in the oven, wrapped in foil on a baking sheet, at 400f for 10-12 minutes. (If coming from a cold refrigerator; please add 5 mins). Ovens vary, so please check your pizza at 5 min intervals.'
+		}
+	];
+</script>
+
+<HomepageInfoBox id="faq" title="FAQ" {faqItems} />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,25 +2,10 @@
 	import HomepageHokkaiDough from '$lib/components/HomepageHokkaiDough.svelte';
 	import HomepageOrderOptions from '$lib/components/HomepageOrderOptions.svelte';
 	import HomepageHero from '$lib/components/HomepageHero.svelte';
-	import HomepageInfoBox from '$lib/components/HomepageInfoBox.svelte';
-
-	import type { FAQItem } from '$lib/types';
-	const faqItems: FAQItem[] = [
-		{
-			question: 'How is the parking?',
-			answer:
-				'Please park in the front parking lot or on the street. Head to the front entrance and select "Schellz Pizza Co" on the call box. Our kitchen is down the hall #7.'
-		},
-		{
-			question: 'How do I reheat Schellz pizzas?',
-			answer:
-				'Due to our long fermentation and our style of dough, you will be surprised to know that Schellz Pizza is quite delicious 2-3 days after purchase.\n\nJust reheat in the oven, wrapped in foil on a baking sheet, at 400f for 10-12 minutes. (If coming from a cold refrigerator; please add 5 mins). Ovens vary, so please check your pizza at 5 min intervals.'
-		}
-	];
+	import HomepageFaq from '$lib/components/HomepageFaq.svelte';
 </script>
 
 <HomepageHero />
 <HomepageOrderOptions />
 <HomepageHokkaiDough />
-
-<HomepageInfoBox id="faq" title="FAQ" {faqItems} />
+<HomepageFaq />
