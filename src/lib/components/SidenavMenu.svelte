25c332f8bf129917fd463a63ca8271a7a842f03f
Thomas Wallsmith <90111415+twallie@users.noreply.github.com>
2023-12-05 06:03:28 +0000

    fix: most navmenu buttons actually work

--- src/lib/components/SidenavMenu.svelte
+++ src/lib/components/SidenavMenu.svelte
@@ -12,6 +12,14 @@
 		scrollTo('faq');
 	};
 
+	const scrollToMenu = () => {
+		scrollTo('menu');
+	};
+
+	const scrollToOrder = () => {
+		scrollTo('order');
+	};
+
 	function scrollTo(id: string) {
 		const element = document.getElementById(id);
 		if (element == null) {
@@ -25,9 +33,9 @@
 <div
 	class="flex flex-col space-y-3 fixed w-full h-full pt-[20%] md:pt-[10%] px-10 bg-slate-200 bg-opacity-[70%] md:w-[50%] md:right-0"
 >
-	<StyledButton text="Order Now" />
+	<StyledButton text="Order Now" onClick={scrollToOrder} />
 	<StyledButton text="Hokkaidough" onClick={scrollToHokkaiDough} />
 	<StyledButton text="Find Us" />
-	<StyledButton text="Full Menu" />
+	<StyledButton text="Full Menu" onClick={scrollToMenu} />
 	<StyledButton text="FAQ" onClick={scrollToFAQ} />
 </div>
