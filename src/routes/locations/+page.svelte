e1135acad64d6cb3a92b6d5cc118d81b284719ea
Thomas Wallsmith <90111415+twallie@users.noreply.github.com>
2023-12-12 10:16:51 +0000

    feat: google maps on /locations

--- src/routes/locations/+page.svelte
+++ src/routes/locations/+page.svelte
@@ -2,14 +2,18 @@
 	import LocationBox from '$lib/components/LocationBox.svelte';
 </script>
 
-<div class="flex flex-col h-screen mx-2">
+<div class="flex flex-col mx-[10%]">
 	<div class="mb-10">
 		<h1 class="text-2xl text-center font-bold mb-2">Primary Location</h1>
 		<LocationBox offersCatering={true} />
 	</div>
 
 	<h1 class="text-xl text-center font-bold mb-2">Other Locations</h1>
-	<div class="flex flex-col space-y-3">
+	<div
+		class="
+        flex flex-col space-y-3 mb-3
+    "
+	>
 		<LocationBox />
 		<LocationBox />
 	</div>
