bfa89f01b450075f823d09df414abddaa8dc8610
Thomas Wallsmith <90111415+twallie@users.noreply.github.com>
2023-12-13 02:21:42 +0000

    fix: spacing adjustments

--- src/lib/components/LocationBox.svelte
+++ src/lib/components/LocationBox.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import { fly } from 'svelte/transition';
-
 	export let offersCatering: boolean = false;
 </script>
 
@@ -14,7 +12,7 @@
 		title="Google Maps"
 		src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d938.2452496870876!2d-118.33118660132882!3d33.97724568708105!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2b763c115764f%3A0x60e42213cad08f42!2sSchellz%20Pizza%20Co!5e0!3m2!1sen!2sus!4v1702374703972!5m2!1sen!2sus"
 		style="border:0;"
-		class="w-full my-2"
+		class="w-full my-2 mx-auto"
 		loading="lazy"
 		referrerpolicy="no-referrer-when-downgrade"
 	/>
