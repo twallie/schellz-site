54890159480a50c740a743eda124ef14197e727d
Thomas Wallsmith <thomaswallsmith@gmail.com>
2023-12-19 02:30:23 -0500

    feat: PR info component on homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,10 +7,15 @@
 	import HomepageCatering from '$lib/components/HomepageCatering.svelte';
 	import HomepageMenu from '$lib/components/HomepageMenu.svelte';
 	import LocationBox from '$lib/components/LocationBox.svelte';
+	import PrInfo from '$lib/components/PRInfo.svelte';
 </script>
 
 <div class="md:mx-[15%] lg:mx-[25%] 2xl:mx-[35%] space-y-1">
 	<HomepageHero />
+	<PrInfo
+		text="Schellz's Pizza was ranked #20 in places to eat in Los Angeles!"
+		link="https://www.yelp.com/collection/XaC45OLZQUFiLvTw5cLbvw/Top-100-Places-to-Eat-in-LA-County-2023?utm_content=Collections&utm_source=ishare"
+	/>
 	<div
 		class="
 		space-y-1
