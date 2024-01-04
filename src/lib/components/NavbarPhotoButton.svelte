da0d378d1a9cc0b74f633898063cfc034cde6d24
twallie <thomaswallsmith@gmail.com>
2024-01-04 05:05:55 -0500

    feat: socials in navbar

---
+++ src/lib/components/NavbarPhotoButton.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let src: string;
+	export let alt: string;
+	export let giveBackground: boolean = false;
+</script>
+
+<button class={giveBackground ? 'bg-white bg-opacity-80 rounded-3xl' : ''}>
+	<img {src} class="h-[75px] my-auto" {alt} />
+</button>
