2a1cb3510a82e3d669db46cf80a73d79b7e8c5a5
twallie <thomaswallsmith@gmail.com>
2024-01-07 07:51:32 -0500

    feat: tomato character placeholders for menu

--- src/lib/components/HomepageMenu.svelte
+++ src/lib/components/HomepageMenu.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import HomepageInfoBox from './HomepageInfoBox.svelte';
 	import MenuButton from './MenuButton.svelte';
+	import Tomato from './Tomato.svelte';
 </script>
 
 <div
@@ -9,16 +9,23 @@
 	class="
 		text-center
 		text-white
-		bg-[#ee2f24] py-4 rounded-lg
+		bg-[#ee2f24] rounded-lg
 		shadow-sm shadow-black
 		h-full
-		pb-6
+
+		flex flex-row justify-evenly py-5
 	"
 >
+	<div class="my-auto">
+		<Tomato />
+	</div>
 	<div class="flex flex-col align-middle">
 		<h1 class="text-2xl font-medium mx-5">Menu</h1>
-		<div class="flex flex-row flex-wrap pt-5 flex-1 h-[80%] xl:mt-0 justify-center">
+		<div class="my-auto">
 			<MenuButton text="View" onclick={() => goto('menu.png')} />
 		</div>
+	</div>
+	<div class="my-auto">
+		<Tomato />
 	</div>
 </div>
