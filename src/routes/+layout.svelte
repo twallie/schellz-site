dc4dd26efa1eeb021531182e535bb9586564752f
twallie <thomaswallsmith@gmail.com>
2024-01-07 13:27:56 -0500

    feat: experimental background

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,8 @@
 	import StaticHeader from '$lib/components/StaticHeader.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 
+	import ExperimentalBackground from '$lib/images/experimental/PINEAPPLE_zoomed_out.png';
+
 	let sidenavClicked: boolean = false;
 	let scrollY = 0;
 
@@ -19,3 +21,24 @@
 <Footer />
 
 <svelte:window bind:scrollY />
+
+<style lang="postcss">
+	:global(body) {
+		background-image: url('experiment.png');
+		background-repeat: repeat;
+		animation-name: move;
+		animation-direction: forward;
+		animation-iteration-count: infinite;
+		animation-duration: 10s;
+		animation-timing-function: linear;
+	}
+
+	@keyframes move {
+		0% {
+			background-position: 0px 0px;
+		}
+		100% {
+			background-position: 1000px 656px;
+		}
+	}
+</style>
