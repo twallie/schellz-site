253c4dab04debd69877cb9884f612d8b11dc619e
twallie <thomaswallsmith@gmail.com>
2024-01-07 14:32:43 -0500

    feat: border for main menu

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,9 @@
 	import PrInfo from '$lib/components/PRInfo.svelte';
 </script>
 
-<div class="mx-1 md:mx-[15%] lg:mx-[20%] space-y-1 bg-white -z-40 bg-opacity-90">
+<div
+	class="mx-1 md:mx-[15%] p-1 md:p-3 rounded-lg space-y-1 bg-slate-200 shadow-sm shadow-red -z-40 bg-opacity-90"
+>
 	<PrInfo
 		text="Schellz's Pizza was ranked #20 in places to eat in Los Angeles!"
 		link="https://www.yelp.com/collection/XaC45OLZQUFiLvTw5cLbvw/Top-100-Places-to-Eat-in-LA-County-2023?utm_content=Collections&utm_source=ishare"
